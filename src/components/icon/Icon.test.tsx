c4e9b540b0bc0d2302e3baeb3b70b16262f1cb35
Justin GALLY <justin.gally@external.engie.com>
2024-01-30 15:39:38 +0100

    chore: remove icon component

--- src/components/icon/Icon.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from "@testing-library/react";
-import {expect, it} from "vitest"
-import {Icon} from "./Icon.tsx";
-
-it('should render correctly', () => {
-    const iconName = 'mock';
-    render(<Icon name={iconName}></Icon>);
-    expect(screen.getByText(iconName)).toBeDefined()
-})
